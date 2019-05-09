b7daaa0f229062b0821527c7a7b13f1d592fbdce
SajithTS <43513034+SajithTS@users.noreply.github.com>
2019-05-09 14:56:19 +0530

    Update layout.component.ts

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
-  templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.sass']
+  templateUrl: './layout.component.html'
 })
 export class LayoutComponent implements OnInit {
   showChildOptios:boolean = false;
