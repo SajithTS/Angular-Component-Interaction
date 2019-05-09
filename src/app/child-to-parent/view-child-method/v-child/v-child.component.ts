67ede9a930bfca8f5f6eb180d4f2203f40176470
SajithTS <43513034+SajithTS@users.noreply.github.com>
2019-05-09 14:53:15 +0530

    Update v-child.component.ts

--- src/app/child-to-parent/view-child-method/v-child/v-child.component.ts
+++ src/app/child-to-parent/view-child-method/v-child/v-child.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-v-child',
-  templateUrl: './v-child.component.html',
-  styleUrls: ['./v-child.component.sass']
+  templateUrl: './v-child.component.html'
 })
 export class VChildComponent implements OnInit {
   constructor() { }
