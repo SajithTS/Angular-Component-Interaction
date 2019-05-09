5ac6638b67912d7acd9a0c2a5d8daeedcd5ccf5a
SajithTS <43513034+SajithTS@users.noreply.github.com>
2019-05-09 14:49:18 +0530

    Update o-child.component.ts

--- src/app/child-to-parent/output-method/o-child/o-child.component.ts
+++ src/app/child-to-parent/output-method/o-child/o-child.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit, Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-o-child',
-  templateUrl: './o-child.component.html',
-  styleUrls: ['./o-child.component.sass']
+  templateUrl: './o-child.component.html'
 })
 export class OChildComponent implements OnInit {
   @Output() messageEvent = new EventEmitter<string>();
