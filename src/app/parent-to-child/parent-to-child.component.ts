99a751d1bb8b5c8d40bea9bcda40334ea6e2bee8
SajithTS <43513034+SajithTS@users.noreply.github.com>
2019-05-09 14:59:07 +0530

    Update parent-to-child.component.ts

--- src/app/parent-to-child/parent-to-child.component.ts
+++ src/app/parent-to-child/parent-to-child.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-parent-to-child',
-  templateUrl: './parent-to-child.component.html',
-  styleUrls: ['./parent-to-child.component.sass']
+  templateUrl: './parent-to-child.component.html'
 })
 export class ParentToChildComponent implements OnInit {
 
